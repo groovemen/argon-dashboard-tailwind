fd8082bce772237cfb4e9568faa6588e46aa4e97
Adrian Bleoju <bleojua98@gmail.com>
2022-07-20 17:36:06 +0300

    refactor: update sticky navbar style

--- build/assets/js/navbar-sticky.js
+++ build/assets/js/navbar-sticky.js
@@ -1,6 +1,9 @@
 // Navbar stick on scroll ++ styles
 
 var navbar = document.querySelector("[navbar-main]");
+const white_elements = navbar.querySelectorAll(".text-white");
+const white_before_elements = navbar.querySelectorAll(".before\\:text-white");
+
 
 window.onscroll = function () {
   let blur = navbar.getAttribute("navbar-scroll");
@@ -10,7 +13,19 @@ window.onscroll = function () {
 function stickyNav() {
   if (window.scrollY >= 5) {
     navbar.classList.add("sticky", "top-[1%]", "backdrop-saturate-[200%]", "backdrop-blur-[30px]", "bg-[hsla(0,0%,100%,0.8)]", "shadow-blur", "z-110");
+    white_elements.forEach(element => {
+      element.classList.remove("text-white")
+    });
+    white_before_elements.forEach(element => {
+      element.classList.remove("before:text-white")
+    });
   } else {
     navbar.classList.remove("sticky", "top-[1%]", "backdrop-saturate-[200%]", "backdrop-blur-[30px]", "bg-[hsla(0,0%,100%,0.8)]", "shadow-blur", "z-110");
+    white_elements.forEach(element => {
+      element.classList.add("text-white")
+    });
+    white_before_elements.forEach(element => {
+      element.classList.add("before:text-white")
+    });
   }
 }
