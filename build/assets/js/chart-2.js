c072df0c68a21afa6f22191c4298777e34f7e481
Adrian Bleoju <bleojua98@gmail.com>
2022-07-20 17:35:50 +0300

    refactor: edit chart style

--- build/assets/js/chart-2.js
+++ build/assets/js/chart-2.js
@@ -1,49 +1,29 @@
-// chart 2
 
-var ctx2 = document.getElementById("chart-line").getContext("2d");
 
-var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+var ctx1 = document.getElementById("chart-line").getContext("2d");
 
-gradientStroke1.addColorStop(1, "rgba(203,12,159,0.2)");
-gradientStroke1.addColorStop(0.2, "rgba(72,72,176,0.0)");
-gradientStroke1.addColorStop(0, "rgba(203,12,159,0)"); //purple colors
+var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
 
-var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
-
-gradientStroke2.addColorStop(1, "rgba(20,23,39,0.2)");
-gradientStroke2.addColorStop(0.2, "rgba(72,72,176,0.0)");
-gradientStroke2.addColorStop(0, "rgba(20,23,39,0)"); //purple colors
-
-new Chart(ctx2, {
+gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
+new Chart(ctx1, {
   type: "line",
   data: {
     labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    datasets: [
-      {
-        label: "Mobile apps",
-        tension: 0.4,
-        borderWidth: 0,
-        pointRadius: 0,
-        borderColor: "#cb0c9f",
-        borderWidth: 3,
-        backgroundColor: gradientStroke1,
-        fill: true,
-        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-        maxBarThickness: 6,
-      },
-      {
-        label: "Websites",
-        tension: 0.4,
-        borderWidth: 0,
-        pointRadius: 0,
-        borderColor: "#3A416F",
-        borderWidth: 3,
-        backgroundColor: gradientStroke2,
-        fill: true,
-        data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
-        maxBarThickness: 6,
-      },
-    ],
+    datasets: [{
+      label: "Mobile apps",
+      tension: 0.4,
+      borderWidth: 0,
+      pointRadius: 0,
+      borderColor: "#5e72e4",
+      backgroundColor: gradientStroke1,
+      borderWidth: 3,
+      fill: true,
+      data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+      maxBarThickness: 6
+
+    }],
   },
   options: {
     responsive: true,
@@ -51,11 +31,11 @@ new Chart(ctx2, {
     plugins: {
       legend: {
         display: false,
-      },
+      }
     },
     interaction: {
       intersect: false,
-      mode: "index",
+      mode: 'index',
     },
     scales: {
       y: {
@@ -64,19 +44,19 @@ new Chart(ctx2, {
           display: true,
           drawOnChartArea: true,
           drawTicks: false,
-          borderDash: [5, 5],
+          borderDash: [5, 5]
         },
         ticks: {
           display: true,
           padding: 10,
-          color: "#b2b9bf",
+          color: '#fbfbfb',
           font: {
             size: 11,
             family: "Open Sans",
-            style: "normal",
-            lineHeight: 2,
+            style: 'normal',
+            lineHeight: 2
           },
-        },
+        }
       },
       x: {
         grid: {
@@ -84,22 +64,21 @@ new Chart(ctx2, {
           display: false,
           drawOnChartArea: false,
           drawTicks: false,
-          borderDash: [5, 5],
+          borderDash: [5, 5]
         },
         ticks: {
           display: true,
-          color: "#b2b9bf",
+          color: '#ccc',
           padding: 20,
           font: {
             size: 11,
             family: "Open Sans",
-            style: "normal",
-            lineHeight: 2,
+            style: 'normal',
+            lineHeight: 2
           },
-        },
+        }
       },
     },
   },
 });
 
-// end chart 2
