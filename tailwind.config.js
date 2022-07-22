5e25e8d81d3b78a13bab1f08a9f1450de61a80d3
Adrian Bleoju <bleojua98@gmail.com>
2022-07-22 11:17:12 +0300

    refactor: update sign in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -662,7 +662,7 @@ module.exports = {
       "soft-xxs": "0 1px 5px 1px #ddd",
       "soft-xs": "0 7px 14px rgba(50,50,93,.1),0 3px 6px rgba(0,0,0,.08)", // button hover
       "soft-sm": "0 .25rem .375rem -.0625rem hsla(0,0%,8%,.12),0 .125rem .25rem -.0625rem hsla(0,0%,8%,.07)",
-      "soft-md": "0 4px 7px -1px rgba(0,0,0,.11),0 2px 4px -1px rgba(0,0,0,.07)",
+      "soft-md": "0 4px 6px rgba(50,50,93,.1),0 1px 3px rgba(0,0,0,.08)",
       "soft-lg": "0 2px 12px 0 rgba(0,0,0,.16)",
       "soft-xl": "0 0 2rem 0 rgba(136,152,170,.15)", //shadow cards
       "soft-2xl": "0 .3125rem .625rem 0 rgba(0,0,0,.12)",
